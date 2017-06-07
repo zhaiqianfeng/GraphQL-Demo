9c795a1bbb70eebf66fcd5afb5c3d47571d2601f
zhaiqianfeng <zhaiqianfeng@163.com>
2017-06-07 14:33:38 +0800

    update

--- java/src/main/java/com/zqf/advance/GraphQL_argument.java
+++ java/src/main/java/com/zqf/advance/GraphQL_argument.java
@@ -4,6 +4,8 @@ import com.zqf.model.User;
 import graphql.GraphQL;
 import graphql.schema.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static graphql.Scalars.GraphQLInt;
@@ -23,17 +25,18 @@ import static graphql.schema.GraphQLObjectType.newObject;
 public class GraphQL_argument {
     public static void main(String[] args) {
         //服务端示例数据
-        User user0=new User();
-        user0.setName("zhaiqianfeng");
-        user0.setSex("男");
-        user0.setIntro("博主，专注于Linux,Java,nodeJs,Web前端:Html5,JavaScript,CSS3");
+        List<User> users =new ArrayList<>();
+        User user=new User();
+        user.setName("zhaiqianfeng");
+        user.setSex("男");
+        user.setIntro("博主，专注于Linux,Java,nodeJs,Web前端:Html5,JavaScript,CSS3");
+        users.add(user);
 
-        User user1=new User();
-        user1.setName("zhaiqianfeng");
-        user1.setSex("男");
-        user1.setIntro("博主，专注于Linux,Java,nodeJs,Web前端:Html5,JavaScript,CSS3");
-
-        User[] users={user0,user1};
+        user=new User();
+        user.setName("zhaiqianfeng");
+        user.setSex("男");
+        user.setIntro("博主，专注于Linux,Java,nodeJs,Web前端:Html5,JavaScript,CSS3");
+        users.add(user);
 
         //定义GraphQL类型
         GraphQLObjectType userType = newObject()//定义User类型
@@ -52,6 +55,7 @@ public class GraphQL_argument {
                 .field(newInputObjectField().name("skills").type(new GraphQLList(GraphQLString)))
                 .build();
 
+        //定义暴露给客户端的查询 query api
         GraphQLObjectType queryType = newObject()//定义暴露给客户端的查询query api
                 .name("userQuery")
                 .field(newFieldDefinition()
@@ -62,7 +66,7 @@ public class GraphQL_argument {
                                 .type(new GraphQLNonNull(GraphQLInt)))
                         .dataFetcher(environment -> {
                             int id=environment.getArgument("id");
-                            return users[id];
+                            return users.get(id);
                         }))
                 .field(newFieldDefinition()
                         .type(new GraphQLList(userType))
@@ -72,20 +76,60 @@ public class GraphQL_argument {
                         }))
                 .build();
 
+    //定义暴露给客户端的查询 mutaion api
+        GraphQLObjectType mutaionType = newObject()//定义暴露给客户端的查询query api
+                .name("userMutation")
+                .field(newFieldDefinition()
+                        .type(new GraphQLList(userType))
+                        .name("addUserByInput")
+                        .argument(newArgument()
+                                .name("userInfo")
+                                .type(new GraphQLNonNull(userInput)))
+                        .dataFetcher(environment -> {
+                            Map<String,Object> userInfoMap=environment.getArgument("userInfo");
+                            User userInfo=new User();
+                            for (String key : userInfoMap.keySet()){
+                                switch (key){
+                                    case "name":
+                                        userInfo.setName(userInfoMap.get("name").toString());
+                                        break;
+                                    case "sex":
+                                        userInfo.setSex(userInfoMap.get("sex").toString());
+                                        break;
+                                    case "intro":
+                                        userInfo.setIntro(userInfoMap.get("intro").toString());
+                                        break;
+                                    case "skills":
+                                        ArrayList<String> skillsList=(ArrayList<String>) userInfoMap.get("skills");
+                                        String[] skills=new String[skillsList.size()];
+                                        userInfo.setSkills(skillsList.toArray(skills));
+                                        break;
+
+                                }
+                            }
+                            users.add(userInfo);
+                            return users;
+                        }))
+                .build();
+
         //创建Schema
         GraphQLSchema schema = GraphQLSchema.newSchema()
                 .query(queryType)
+                .mutation(mutaionType)
                 .build();
 
         //测试输出
         GraphQL graphQL = GraphQL.newGraphQL(schema).build();
-
         //根据Id查询用户
         Map<String, Object> result = graphQL.execute("{user(id:0){name,sex,intro}}").getData();
         System.out.println(result);
 
         //查询用户列表
         result = graphQL.execute("{users{name,sex,intro}}").getData();
+        System.out.println(result);
+
+        //添加用户列表
+        result = graphQL.execute("mutation{addUserByInput(userInfo:{name:\"test2User\",sex:\"男\",intro:\"简介\",skills:[\"java\",\"nodejs\"]}){name　sex　intro}}").getData();
         System.out.println(result);
     }
 }
