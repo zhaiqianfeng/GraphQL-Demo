85e79a2bfc0b2b70f2651c42c1dbd67a43ae0e1c
zhaiqianfeng <zhaiqianfeng@163.com>
2017-06-08 14:19:20 +0800

    update

---
+++ javascript/advance/GraphiQL-directive.js
@@ -0,0 +1,140 @@
+/**
+ * Created by zhaiqianfeng on 6/5/17.
+ * 演示GraphQL api如何使用指令(directive)
+ *
+ * blog: www.zhaiqianfeng.com
+ */
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var {
+    graphql,
+    GraphQLList,
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLFloat,
+    GraphQLEnumType,
+    GraphQLNonNull,
+    GraphQLInterfaceType,
+    GraphQLInputObjectType
+} = require('graphql');
+
+//服务端示例数据
+var animals=[
+    {
+        name: 'dog',
+        legs: 4
+    },
+    {
+        name: 'fish',
+        tailColor:'red'
+    },
+];
+
+//定义schema及resolver
+const Animal = new GraphQLInterfaceType({
+    name: 'Animal',
+    description: '接口',
+    fields: () => ({
+        name: {type: new GraphQLNonNull(GraphQLString)},
+    }),
+    /* resolveType:function (obj) {
+     if(obj.legs) {
+     return Dog;
+     }else if(obj.tailColor){
+     return Fish;
+     }else{
+     return null;
+     }
+     }*/
+});
+
+const Dog = new GraphQLObjectType({
+    name: 'Dog',
+    interfaces: [Animal],
+    description: '狗狗实体',
+    fields: () => ({
+        name: {type: new GraphQLNonNull(GraphQLString)},
+        legs: {type: new GraphQLNonNull(GraphQLInt)},
+    }),
+    isTypeOf:obj=>obj.legs,
+});
+
+const Fish=new GraphQLObjectType({
+    name:'Fish',
+    interfaces:[Animal],
+    description:"鱼儿实体",
+    fields: () => {
+        return ({
+            name: {type: new GraphQLNonNull(GraphQLString)},
+            tailColor: {type: new GraphQLNonNull(GraphQLString)},
+        });
+    },
+    isTypeOf:obj=>obj.tailColor,
+});
+const Query=new GraphQLObjectType({
+    name:'AnimalQuery',
+    description:'动物信息查询',
+    fields:()=>({
+        animals:{
+            type:new GraphQLList(Animal),
+            description:'查询全部动物列表',
+            resolve:function () {
+                return animals;
+            }
+        },
+        animalSearch:{
+            type:Animal,
+            args: {
+                text: {type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve:function (source,{text}) {
+                if (text == 'dog') {
+                    return animals[0];
+                }else{
+                    return animals[1];
+                }
+            }
+        }
+    }),
+});
+const schema = new GraphQLSchema({
+    types: [Dog, Fish,Animal],
+    query: Query
+});
+
+//测试解析请求
+graphql(schema, 'query(\
+    $noWithDog:Boolean!,\
+        $withName:Boolean!,\
+        $withFish:Boolean!\
+    ){\
+        animals{\
+            name @include(if:$withName)\
+        ... dogQuery @skip(if:$noWithDog)\
+        ... on Fish @include(if:$withFish){\
+                tailColor\
+            }\
+        }\
+    }\
+    fragment dogQuery on Dog{\
+        legs\
+    }',null,null,
+    {
+        "noWithDog": true,
+        "withName": true,
+        "withFish": true
+    }).then((response) => {
+    console.log(response);
+});
+
+
+//启动服务用于测试
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true, //启用GraphiQL
+}));
+
+app.listen(4000, () => console.log('请在浏览器中打开地址：localhost:4000/graphql'));
